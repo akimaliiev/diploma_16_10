ce617a24017be9b25bbb42b7eb0c0ffc5f1a1c4c
Sultan Akimaliyev <sakimaliev1@gmail.com>
2024-05-02 21:44:26 +0500

    first commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:diploma_16_10/components/my_button.dart';
 import 'package:diploma_16_10/components/my_textfield.dart';
+import 'package:diploma_16_10/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,6 +9,8 @@ class LoginPage extends StatelessWidget {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
+  void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -14,6 +18,7 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(height: 50,),
               Icon(
@@ -60,6 +65,73 @@ class LoginPage extends StatelessWidget {
                   ],
                 ),
               ),
+
+              const SizedBox(height: 25,),
+
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 50,),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: .7,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                        ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: .7,
+                        color: Colors.grey[600],
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 20,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(imagePath: 'lib/images/google_logo.png'),
+                  SizedBox(width: 10,),
+
+                  SquareTile(imagePath: 'lib/images/android_logo.png'),
+                ],
+              ),
+
+              const SizedBox(height: 30,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 5,),
+                  Text(
+                    'Register now!',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
     
 
         
